6f92834f4ff9deb8de04eac590234389db98fd17
Javiistacks <javiann.marrero@icloud.com>
2021-05-10 19:10:42 -0500

    loaded all files

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,23 @@
-CREATE myTreasures_db;
+DROP DATABASE IF EXISTS shoes_db;
 
-CREATE TABLE sellers (
-    sellers_id INT NOT NULL,
-    address_id VARCHAR(100) NOT NULL,
-    products VARCHAR(100) NOT NULL,
-    date_id DATE NOT NULL,
-    createdAt TIMESTAMP NOT NULL,
-    PRIMARY KEY(sellers_id));
+CREATE DATABASE shoes_db;
 
-CREATE TABLE buyers_cart (
-    buyers_cart_id INT NOT NULL,
-    name_id VARCHAR (100) NOT NULL,
-    products VARCHAR(100) NOT NULL,
-    createdAt TIMESTAMP NOT NULL,
-    PRIMARY KEY (buyers_cart_id)
-)
+
+
+-- CREATE myTreasures_db;
+
+-- CREATE TABLE sellers (
+--     sellers_id INT NOT NULL,
+--     address_id VARCHAR(100) NOT NULL,
+--     products VARCHAR(100) NOT NULL,
+--     date_id DATE NOT NULL,
+--     createdAt TIMESTAMP NOT NULL,
+--     PRIMARY KEY(sellers_id));
+
+-- CREATE TABLE buyers_cart (
+--     buyers_cart_id INT NOT NULL,
+--     name_id VARCHAR (100) NOT NULL,
+--     products VARCHAR(100) NOT NULL,
+--     createdAt TIMESTAMP NOT NULL,
+--     PRIMARY KEY (buyers_cart_id)
+-- )
